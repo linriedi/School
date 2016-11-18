a7b29450b85a0329e1c9d5a6d0ef36ee5f2172f6
Riedi Linus <linriedi@hotmail.com>
2016-11-18 18:53:49 +0100

    Port tests from local

---
+++ AlgTsp/tests/NearestNeighbourTests.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections.Generic;
+using TspLib;
+using TspLib.Algos;
+using Xunit;
+
+namespace tests
+{
+    public class NearestNeighbourTests
+    {
+        [Fact]
+        public void CalculateLength()
+        {
+            var first = new Point(2, 0, 2.5);
+            var points = new List<Point>
+            {
+                new Point(4, 0, 1.5),
+                new Point(3, 0, 0.5),
+                new Point(1, 0.5, 2),
+                new Point(5, 0.5, 1),
+                new Point(6, 0.5, 0)
+            };
+
+            var lenght = NearestNeighbour.CalculateLength(first, points);
+        }
+
+        [Fact]
+        public void CalculateLength_OnlyFew()
+        {
+            var first = new Point(2, 0, 2.5);
+            var points = new List<Point>
+            {
+                new Point(1, 0.5, 2),
+                new Point(4, 0, 1.5),
+            };
+
+            var lenght = NearestNeighbour.CalculateLength(first, points);
+        }
+
+        [Fact]
+        public void CalculateLength_OnlyOne()
+        {
+            var first = new Point(2, 0, 2.5);
+            var points = new List<Point>
+            {
+                new Point(1, 0.5, 2)
+            };
+
+            var lenght = NearestNeighbour.CalculateLength(first, points);
+        }
+    }
+}
---
+++ AlgTsp/tests/PilotMethodTests.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections.Generic;
+using TspLib;
+using Xunit;
+
+namespace tests
+{
+    public class PilotMethodTests
+    {
+        [Fact]
+        public void CalculateLength()
+        {
+            var points = new List<Point>
+            {
+                new Point(3, 0, 0),
+                new Point(6, 1, 0),
+                new Point(4, 2, 0),
+                new Point(1, 2, 2),
+                new Point(5, 1, 2),
+                new Point(2, 0, 2)
+            };
+
+            var test = new PilotMethod().Solve(new Instance("", "", points));
+        }
+
+        [Fact]
+        public void CalculateLength_OnlyTwo()
+        {
+            var points = new List<Point>
+            {
+                new Point(4, 2, 0),
+                new Point(1, 2, 2),
+                new Point(5, 1, 2)
+            };
+
+            var test = new PilotMethod().Solve(new Instance("", "", points));
+        }
+
+        [Fact]
+        public void CalculateLength_OnlyFour()
+        {
+            var points = new List<Point>
+            {
+                new Point(4, 2, 0),
+                new Point(1, 2, 2),
+                new Point(5, 1, 2),
+                new Point(2, 1, 0)
+            };
+
+            var test = new PilotMethod().Solve(new Instance("", "", points));
+        }
+    }
+}
